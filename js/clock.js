f46ac382d58453f69593430944801d30a9e08b22
Tom Drapes <tdrapes@myune.edu.au>
2017-11-22 15:37:04 +1000

    fixed bugs

--- js/clock.js
+++ js/clock.js
@@ -17,34 +17,54 @@ $(document).ready(function(){
     });
 
     function myTimer(){
-        
-        if(seconds <= 0){
-            minutes -= 1;
-            seconds = 60;
+        var toggle = true;
+        if(minutes <=0 && seconds <= 0){                        
+            $("#timer").css("font-size", "54px");            
+            $("#timer").html("TIME UP");
+            toggle = false;
+            
         }
-        seconds -= 1;
-        $("#timer").html(minutes.toString()+":"+seconds.toString());
+        if(toggle){
+            if(seconds <= 0){
+                seconds = 60;
+                minutes -= 1;             
+            }
+            seconds -= 1;
+            if(minutes < 10 && seconds < 10){
+                $("#timer").html("0"+minutes.toString()+":0"+seconds.toString());
+            }else if(minutes < 10){
+                $("#timer").html("0"+minutes.toString()+":"+seconds.toString());
+            }else if(seconds < 10){
+                $("#timer").html(minutes.toString()+":0"+seconds.toString());
+            }else{
+                $("#timer").html(minutes.toString()+":"+seconds.toString());
+            }
+        }
 
     }
 
     $("#minus").on("click", function(){        
         if(minutes > 1){
-            minutes -= 1;
-            clearInterval(timer);
-            clicked = false;
-            seconds = 0;
-            $("#start").html("Start");
-            $("#timer").html(minutes.toString()+":00");
+            minutes -= 1;            
+            reset();
         }
     });
     $("#plus").on("click", function(){
         if(minutes < 60){
             minutes += 1;
-            clearInterval(timer);
-            clicked = false;
-            seconds = 0;
-            $("#start").html("Start");
-            $("#timer").html(minutes.toString()+":00");
+            reset();            
         }
     });
+
+    function reset(){
+        clearInterval(timer);
+        clicked = false;
+        seconds = 0;
+        $("#start").html("Start");
+        if(minutes < 10){
+            $("#timer").html("0"+minutes.toString()+":00");
+        }else{
+            $("#timer").html(minutes.toString()+":00");
+        }
+    }
 });
