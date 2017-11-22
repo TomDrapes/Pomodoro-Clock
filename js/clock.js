3c6add2af2c49cc34c7c453f10d3a160e2245c7f
Tom Drapes <tdrapes@myune.edu.au>
2017-11-22 14:28:31 +1000

    added core functionality

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,50 @@
+$(document).ready(function(){
+    //var timer = setInterval(myTimer, 1000);
+    var minutes = 25;
+    var seconds = 0;
+    var clicked = false;
+    var timer = 0;
+    $("#start").on("click", function(){
+        if(!clicked){
+            timer = setInterval(myTimer, 1000);            
+            $("#start").html("Stop");
+            clicked = true;
+        }else{            
+            clearInterval(timer);
+            $("#start").html("Start");
+            clicked = false;
+        }
+    });
 
+    function myTimer(){
+        
+        if(seconds <= 0){
+            minutes -= 1;
+            seconds = 60;
+        }
+        seconds -= 1;
+        $("#timer").html(minutes.toString()+":"+seconds.toString());
+
+    }
+
+    $("#minus").on("click", function(){        
+        if(minutes > 1){
+            minutes -= 1;
+            clearInterval(timer);
+            clicked = false;
+            seconds = 0;
+            $("#start").html("Start");
+            $("#timer").html(minutes.toString()+":00");
+        }
+    });
+    $("#plus").on("click", function(){
+        if(minutes < 60){
+            minutes += 1;
+            clearInterval(timer);
+            clicked = false;
+            seconds = 0;
+            $("#start").html("Start");
+            $("#timer").html(minutes.toString()+":00");
+        }
+    });
+});
